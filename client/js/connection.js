a7486895ece09276a98d39d9aead8de3f487ac35
Justinas Ruslys <jjustinas.ruslys@gmail.com>
2021-06-22 19:50:56 +0300

    Initial commit

---
+++ client/js/connection.js
@@ -0,0 +1,7 @@
+const connectionUrl = "http://localhost:5000";
+const connectBtn = document.getElementById("connectBtn");
+
+connectBtn.onclick = function() {
+    var inputs = document.getElementById("inputFields");
+    inputs.innerHTML = "";
+}
