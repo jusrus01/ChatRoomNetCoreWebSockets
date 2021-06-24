c73acfd54cee84700165ca4c72c39ae79716e4dd
Justinas Ruslys <jjustinas.ruslys@gmail.com>
2021-06-24 17:48:52 +0300

    Implemented simple json communication between client and server

--- server/Middlewares/WebSocketServerMiddleware.cs
+++ server/Middlewares/WebSocketServerMiddleware.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace Server.Middlewares
 {
@@ -30,7 +31,21 @@ namespace Server.Middlewares
                 await SendConnectionId(webSocket, id);
                 await Receive(webSocket, async (result, buffer) =>
                 {
+                    if(result.MessageType == WebSocketMessageType.Text)
+                    {
+                        string receivedMsg = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                        await RouteJSONMessageAsync(receivedMsg);
+                    }
+                    else if(result.MessageType == WebSocketMessageType.Close)
+                    {
+                        string id = _manager.Sockets.FirstOrDefault(soc => soc.Value == webSocket).Key;
 
+                        WebSocket socket;
+                        _manager.Sockets.TryRemove(id, out socket);
+
+                        await socket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+                    }
+                    return;
                 });
 
             }
@@ -40,10 +55,49 @@ namespace Server.Middlewares
                 await _next(context);
             }
         }
+        
+        private async Task RouteJSONMessageAsync(string msg) // might need to add JSON validation
+        {
+            var routeOb = JsonConvert.DeserializeObject<dynamic>(msg);
+            Guid guidOutput;
+
+            if(Guid.TryParse(routeOb.To.ToString(), out guidOutput))
+            {
+                string toGuid = routeOb.To;
+                var recipientSocket = _manager.Sockets.FirstOrDefault(soc => soc.Key == toGuid);
+
+                if(recipientSocket.Value != null)
+                {
+                    if(recipientSocket.Value.State == WebSocketState.Open)
+                    {
+                        string json = JsonConvert.SerializeObject(new { Message = routeOb.Message.ToString() }, Formatting.Indented);
+                        await recipientSocket.Value.SendAsync(Encoding.UTF8.GetBytes(json), WebSocketMessageType.Text,
+                            true, CancellationToken.None);
+                    }
+                }
+            }
+            else
+            {
+                string json = JsonConvert.SerializeObject(new { Message = routeOb.Message.ToString() }, Formatting.Indented);
+                byte[] jsonData = Encoding.UTF8.GetBytes(json);
+
+                foreach(var soc in _manager.Sockets)
+                {
+                    if(soc.Value.State == WebSocketState.Open)
+                    {
+                        
+                        await soc.Value.SendAsync(jsonData, WebSocketMessageType.Text,
+                            true, CancellationToken.None);
+                    }
+                }
+            }
+        }
 
         private async Task SendConnectionId(WebSocket socket, string id)
-        {
-            byte[] bytes = Encoding.UTF8.GetBytes(id);
+        {   
+            string json = JsonConvert.SerializeObject(new { ConnectionId = id }, Formatting.Indented);
+            byte[] bytes = Encoding.UTF8.GetBytes(json);
+            
             await socket.SendAsync(bytes, WebSocketMessageType.Text, true, CancellationToken.None);
         }
 
